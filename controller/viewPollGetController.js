da079dec9eb4a7faf264220c634875f516997351
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-06-04 15:32:56 +0600

    viewPollGetController created

--- controller/viewPollGetController.js
+++ controller/viewPollGetController.js
@@ -1,5 +1,29 @@
-exports.viewPollGetController = (req, res, next) => {
+const Poll = require('./../model/Poll');
+
+exports.viewPollGetController = async (req, res, next) => {
   const id = req.params.id;
-  console.log(id);
-  res.render('home');
+  // console.log(id);
+  try {
+    const poll = await Poll.findById(id);
+    // console.log(poll);
+    let options = [...poll.options];
+    // console.log(options);
+    let result = [];
+    options.forEach((option) => {
+      let percantage = (option.vote * 100) / poll.totalVote;
+
+      // result.push({
+      //   option._doc,
+      //   percantage
+      // })
+      result.push({
+        ...option._doc,
+        percantage: percantage ? percantage : 0,
+      });
+      console.log(result);
+    });
+    res.render('singlePoll', { poll, result });
+  } catch (e) {
+    console.log(e);
+  }
 };
--- controller/viewPollPostController.js
+++ controller/viewPollPostController.js
@@ -1,3 +1,11 @@
-exports.viewPollPostController = (req, res, next) => {
-  res.render("home");
+const Poll = require('./../model/Poll');
+exports.viewPollPostController = async (req, res, next) => {
+  let id = req.params.id;
+  let optionId = req.body.option;
+  try {
+    const polls = await Poll.find();
+    res.render('polls', { polls });
+  } catch (e) {
+    console.log(e);
+  }
 };
